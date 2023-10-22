68beaf810a53340dc91cfe1da2dbdeca05cf65b5
wzwy <wzwy@tutanota.com>
2023-10-22 12:52:38 +0800

    wip: add documentation for success probability function and a basic test

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,27 @@
-//! # wotr-utils
+//! # wotr-combat-advice
 //!
 //! Utilities to help win combats in Pathfinder: Wrath of the Righteous (WOTR).
 //!
-//! ## Current Advice
-//! 1. What is the probability of X hitting Y? This can be used for the  general case of roll + modifiers VS difficulty check.
+//! ### Current Advice Available
+//! 1. What is the probability of X hitting Y? This can be used for the general case of roll + modifiers VS difficulty check.
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+/// Finds the probability of succeeding against a difficulty value. Note that the difficulty value can be any value that the modifiers can be used against ,e.g. DC and AC,
+/// since mechanically they all work the same way even if the context is different.
+///
+/// Order of the first two arguments is important here. The first argument should be the modifiers of the challenger (the person or situation presented with the difficulty)
+/// while the second argument should be the difficulty check. Mixing up the two might give you the wrong probability.
+///
+/// **Example**
+/// ```
+/// let success_chance = find_success_prob_against_difficulty(10, 20, 20);
+/// assert_eq!(success_chance, 0.8);
+/// ```
+pub fn find_success_prob_against_difficulty(
+    challenger_modifier: u32,
+    difficulty_check: u32,
+    dice_size: u32,
+) -> f32 {
+    todo!("currently implementing");
 }
 
 #[cfg(test)]
@@ -15,7 +30,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let result = find_success_prob_against_dc(10, 20, 20);
+        assert_eq!(result, 0.45);
     }
 }
